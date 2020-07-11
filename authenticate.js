a19b48b90e3a4948b49b35a2a5d20acf492b8567
Tejas-G <Tejas-G@users.noreply.github.com>
2020-07-12 00:10:00 +0530

    assignment 3

--- authenticate.js
+++ authenticate.js
@@ -1,38 +1,48 @@
 var passport = require('passport');
-var localstrategy = require('passport-local').Strategy;
-var jwtstrategy = require('passport-jwt').Strategy;
-var passport = require('passport');
-var extractjwt = require('passport-jwt');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/users');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
 var config = require('./config');
-var User = require('./models/users');
 
-passport.use(new localstrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
+exports.getToken = function (user) {
     return jwt.sign(user, config.secretKey, {expiresIn: 10800});
-};
+}
 
 var opts = {};
-opts.jwtFromRequest = extractjwt.ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtpassport = passport.use(new jwtstrategy(opts, 
-    (jwt_payload, done) => {
-        console.log('JWT payload: ', jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
-            if(err) {
-                return done(err, false);
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT Payload : ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) => {
+        if (err) {
+              return done(err, false);
+        }
+          else if (user) {
+              return done(null, user);
+        }
+          else {
+              return done(null, false);
             }
-            else if(user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
-            }
-        });
-    }));
+    });
+
+}));
+
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyAdmin =   function (req, res, next) {
+    if (req.user.admin) {
+        next();
+    }
+      else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+}
